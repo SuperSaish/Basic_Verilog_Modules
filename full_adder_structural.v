b498f37a031b5fa7b8bbf9e1596e1d652a246d84
SuperSaish <129974287+SuperSaish@users.noreply.github.com>
2023-09-19 18:18:20 +0530

    Create full_adder_structural.v

---
+++ full_adder_structural.v
@@ -0,0 +1,17 @@
+
+module full_adder(
+input a,b,c,
+output sum,cout
+    );
+  wire w1,c1,c2,c3,out1;
+  xor x1(w1,a,b);
+  xor x2(sum,w1,c);
+  
+  and a1(c1,a,b);
+  and a2(c2,b,c);
+  and a3(c3,a,c);
+  
+  or o1(out1,c1,c2);
+  or o2(cout,out1,c3);
+    
+endmodule
