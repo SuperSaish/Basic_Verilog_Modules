9e3c548412515ee4dcf33c481c3291e8915f4534
SuperSaish <129974287+SuperSaish@users.noreply.github.com>
2023-09-20 00:07:06 +0530

    Update demux_8x1_using_2x1.v

--- demux_8x1_using_2x1.v
+++ demux_8x1_using_2x1.v
@@ -1,4 +1,4 @@
-module demux_2x1(
+module demux_1x2(
   input wire sel,
   input wire din,
   output wire dout_0,
@@ -8,21 +8,21 @@ module demux_2x1(
   assign dout_1 = (sel) ? din : 1'b0;
 endmodule
 
-module demux_8x1(
+module demux_1x8(
   input wire [2:0] sel,
   input wire din,
   output wire [7:0]out
 );
   wire w1,w2,w3,w4,w5,w6;
   
-  demux_2x1 M1(sel[2],din,w1,w2);
+  demux_1x2 M1(sel[2],din,w1,w2);
   
-  demux_2x1 M2(sel[1],w1,w3,w4);
-  demux_2x1 M3(sel[1],w2,w5,w6);
+  demux_1x2 M2(sel[1],w1,w3,w4);
+  demux_1x2 M3(sel[1],w2,w5,w6);
   
-  demux_2x1 M4(sel[0],w3,out[0],out[1]);
-  demux_2x1 M5(sel[0],w4,out[2],out[3]);
-  demux_2x1 M6(sel[0],w5,out[4],out[5]);
-  demux_2x1 M7(sel[0],w6,out[6],out[7]);
+  demux_1x2 M4(sel[0],w3,out[0],out[1]);
+  demux_1x2 M5(sel[0],w4,out[2],out[3]);
+  demux_1x2 M6(sel[0],w5,out[4],out[5]);
+  demux_1x2 M7(sel[0],w6,out[6],out[7]);
   
 endmodule
